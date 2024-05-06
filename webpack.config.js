4d617431aa4d29bf7b296941bf058a412e924817
Denys Sobol <denys.sobol@strikersoft.com>
2024-05-06 13:40:38 +0100

    KDL-1185: install preact/compat

--- webpack.config.js
+++ webpack.config.js
@@ -214,6 +214,7 @@ module.exports = {
       react: 'preact/compat',
       'react-dom/test-utils': 'preact/test-utils',
       'react-dom': 'preact/compat',
+      "react/jsx-runtime": "preact/jsx-runtime"
     },
   },
   optimization: {
